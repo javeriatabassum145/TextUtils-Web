cb755060dd2685b4f84e00e734028f41e5511a05
javeriatabassum145 <javeria.tabassum145@gmail.com>
2022-04-18 03:21:36 +0500

    Updates

---
+++ precache-manifest.b466fa0f1da9162fd71f278993c34f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7254825793f5708790a",
+    "url": "/TextUtils-Web/static/css/main.e5408d74.chunk.css"
+  },
+  {
+    "revision": "c7254825793f5708790a",
+    "url": "/TextUtils-Web/static/js/main.c7254825.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/TextUtils-Web/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "e852ef3f5606f256eae9",
+    "url": "/TextUtils-Web/static/js/2.e852ef3f.chunk.js"
+  },
+  {
+    "revision": "026ae60241c4a9fd762f",
+    "url": "/TextUtils-Web/static/js/runtime~main.026ae602.js"
+  },
+  {
+    "revision": "391be445c0be01737c2047f4276fadfa",
+    "url": "/TextUtils-Web/index.html"
+  }
+];
