62f99453250e60386bd104cdf9d856ebae416678
javeriatabassum145 <javeria.tabassum145@gmail.com>
2022-04-18 03:14:31 +0500

    TextUtils - React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
+  const [mode,setMode] = useState('pink');
+
+  const toggleBtn = () =>{
+    if (mode==='pink'){
+      setMode('dark')
+    }
+    else{
+      setMode('pink')
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode = {mode} toggleBtn = {toggleBtn} />
+      <TextForm heading="Enter the text below to analyze"/>
+    </>
   );
 }
 
